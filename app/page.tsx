18bcc0a4f058459bbb58756efadaf35645352844
fin.kingma <fin.kingma@ns.nl>
2025-09-23 19:33:03 +0200

    state toch als parsed string loggen

--- app/page.tsx
+++ app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
       if (data.reply.includes("**GAME_STATE**")) {
         const userReply = data.reply.split("**GAME_STATE**")[0]
         const newGameState = data.reply.split("**GAME_STATE**")[1]
-        console.log('received game state: \n', JSON.parse(newGameState))
+        console.log('received game state: \n', JSON.stringify(JSON.parse(newGameState), null, 2))
         localStorage.setItem('gameState', newGameState);
         data.reply = userReply
       }
